b7ba8d8945b356a94b0f98a2a64dc667dba6aa47
chocoberu <43817454+chocoberu@users.noreply.github.com>
2022-09-08 23:41:00 +0900

    NEW : Zombie Chasing 상태 추가

--- Assets/Scripts/Enemy/ZombieBoss.cs
+++ Assets/Scripts/Enemy/ZombieBoss.cs
@@ -7,7 +7,7 @@ using UnityEngine.AI;
 public enum ZombieBossState
 {
     Idle = 0,
-    Detect,
+    Patrol,
     Chasing,
     Attack,
     Dead
@@ -23,11 +23,26 @@ public class ZombieBoss : MonoBehaviourPun, IPunObservable
     private Animator animator;
     private Renderer zombieRenderer;
     private Rigidbody zombieRigidbody;
+    private ZombieHealth zombieHealth;
 
     private ZombieBossState state;
     public ZombieBossState State { get; }
 
     private FSM<ZombieBossState> fsm;
+
+    private HealthComponent target;
+
+    // 공격 관련
+    private float damage = 30.0f;
+    private float timeBetAttack = 5.0f;
+    private float attackRange = 2.0f;
+    private float detectRange = 20.0f;
+
+    private float rotSpeed = 30.0f;
+
+    // 동기화 관련
+    private Vector3 serializedPosition;
+    private Quaternion serializedRotation;
 
     private void Awake()
     {
@@ -38,8 +53,12 @@ public class ZombieBoss : MonoBehaviourPun, IPunObservable
         pathFinder = GetComponent<NavMeshAgent>();
         zombieRenderer = GetComponent<Renderer>();
         zombieRigidbody = GetComponent<Rigidbody>();
+        zombieHealth = GetComponent<ZombieHealth>();
 
         fsm = new FSM<ZombieBossState>(this);
+        pathFinder.isStopped = true;
+
+        zombieHealth.OnDeath += OnDead;
     }
 
     // Start is called before the first frame update
@@ -56,12 +75,156 @@ public class ZombieBoss : MonoBehaviourPun, IPunObservable
 
     public void Idle_Enter()
     {
-        Debug.Log("Idle Enter");
+        state = ZombieBossState.Idle; 
     }
 
     public void Idle_Update()
     {
-        Debug.Log("Idle update!");
+        DetectTarget();
+    }
+
+    public void Idle_Exit()
+    {
+
+    }
+
+    private void DetectTarget()
+    {
+        if(false == PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+
+        // Overlap을 통해 candidate 리스트 가져오기
+        Collider[] colliders = Physics.OverlapSphere(transform.position, detectRange, targetLayer);
+
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            HealthComponent entity = colliders[i].GetComponent<HealthComponent>();
+            if(null == entity || true == entity.Dead || this == entity)
+            {
+                continue;
+            }
+
+            if(null == target)
+            {
+                target = entity;
+            }
+            else
+            {
+                if(Vector3.Distance(target.transform.position, transform.position) > 
+                    Vector3.Distance(entity.transform.position, transform.position))
+                {
+                    target = entity;
+                }
+            }
+        }
+
+        if(null != target)
+        {
+            photonView.RPC("TransitionState", RpcTarget.All, (int)ZombieBossState.Chasing);
+        }
+    }
+
+    public void Patrol_Enter()
+    {
+        photonView.RPC("SetMove", RpcTarget.All, true);
+    }
+
+    public void Patrol_Update()
+    {
+
+    }
+
+    public void Patrol_Exit()
+    {
+        photonView.RPC("SetMove", RpcTarget.All, false);
+    }
+
+    private void MoveToTarget()
+    {
+        if(false == PhotonNetwork.IsMasterClient)
+        {
+            transform.position = Vector3.Lerp(transform.position, serializedPosition, Time.deltaTime * pathFinder.speed);
+            transform.rotation = Quaternion.Slerp(transform.rotation, serializedRotation, Time.deltaTime * rotSpeed);
+            return;
+        }
+
+        if(null == target)
+        {
+            photonView.RPC("TransitionState", RpcTarget.All, (int)ZombieBossState.Idle);
+            return;
+        }
+
+        pathFinder.SetDestination(target.transform.position);
+
+        // 공격 범위 내에 들었을 때
+        if(Vector3.Distance(target.transform.position, transform.position) <= attackRange + 1.0f)
+        {
+            pathFinder.isStopped = true;
+
+            Quaternion rot = Quaternion.LookRotation(target.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, rotSpeed * Time.deltaTime);
+
+            fsm.Transition(ZombieBossState.Attack);
+        }
+    }
+
+    public void Chasing_Enter()
+    {
+        Debug.Log("Chasing enter");
+        pathFinder.isStopped = false;
+
+        photonView.RPC("SetMove", RpcTarget.All, true);
+    }
+
+    public void Chasing_Update()
+    {
+        MoveToTarget();
+    }
+
+    public void Chasing_Exit()
+    {
+        photonView.RPC("SetMove", RpcTarget.All, false);
+    }
+
+    public void Attack_Enter()
+    {
+        photonView.RPC("PlayNormalAttack", RpcTarget.All);
+        fsm.Transition(ZombieBossState.Chasing);
+    }
+
+    public void Dead_Enter()
+    {
+        pathFinder.isStopped = true;
+    }
+
+    [PunRPC]
+    private void TransitionState(int nextState)
+    {
+        fsm.Transition((ZombieBossState)nextState);
+    }
+
+    [PunRPC]
+    private void SetMove(bool value)
+    {
+        animator.SetBool("Move", value);
+    }
+
+    [PunRPC]
+    private void PlayNormalAttack()
+    {
+        animator.SetTrigger("NormalAttack");
+    }
+
+    private void HitCheck()
+    {
+
+    }
+
+    private void OnDead()
+    {
+        fsm.Transition(ZombieBossState.Dead);
     }
 
     public virtual void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -73,8 +236,8 @@ public class ZombieBoss : MonoBehaviourPun, IPunObservable
         }
         else
         {
-            transform.position = (Vector3)stream.ReceiveNext();
-            transform.rotation = (Quaternion)stream.ReceiveNext();
+            serializedPosition = (Vector3)stream.ReceiveNext();
+            serializedRotation = (Quaternion)stream.ReceiveNext();
         }
     }
 
